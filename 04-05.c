b6258c9fce03965872b4eca8b6e92d296fa7854a
Dajan L <62118597+lyttdaj20@users.noreply.github.com>
2021-09-09 10:50:02 -0700

    Add files via upload

---
+++ 04-05.c
@@ -0,0 +1,26 @@
+
+// 4.5
+
+#include <stdio.h>
+
+float f;
+
+int d;
+
+char c;
+
+int main()
+{
+    
+    f = 5.31;
+    
+    d = 6;
+    
+    c = 'a';
+    
+    printf("Float as Integer: %d\n", f);
+    printf("Integer as Float: %f\n", d);
+    printf("Character as Integer: %d\n", c);
+
+    return 0;
+}
