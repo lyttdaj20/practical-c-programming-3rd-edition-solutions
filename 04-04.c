e92b15122595ed8b8615229a64253be6808e1e9e
Dajan L <62118597+lyttdaj20@users.noreply.github.com>
2021-09-09 10:25:31 -0700

    Add files via upload

---
+++ 04-04.c
@@ -0,0 +1,17 @@
+
+// 4.4
+
+#include <stdio.h>
+
+int main()
+{
+    printf("*   * ***** *     *     *****\n");
+    printf("*   * *     *     *     *   *\n");
+    printf("*   * *     *     *     *   *\n");
+    printf("***** ***** *     *     *   *\n");
+    printf("*   * *     *     *     *   *\n");
+    printf("*   * *     *     *     *   *\n");
+    printf("*   * ***** ***** ***** *****");
+
+    return 0;
+}
